33bb290916415a27346fbfea080ea4f465f47b82
RamPakanayev <rampakanayev@gmail.com>
2023-01-24 20:29:39 +0200

    erase existing project

--- cost-manager-frontend/src/components/App.jsx
+++ cost-manager-frontend/src/components/App.jsx
@@ -1,8 +1,4 @@
 import React from "react";
-import SpentTotal from "./SpentTotal";
-import SpentList from "./SpentList";
-import AddForm from "./AddForm";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
