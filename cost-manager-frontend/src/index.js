c517c9c506afba9ba6c40701f4cb5ef3432cea1a
RamPakanayev <rampakanayev@gmail.com>
2023-01-25 19:54:47 +0200

    Update index.js
    adding comments

--- cost-manager-frontend/src/index.js
+++ cost-manager-frontend/src/index.js
@@ -1,10 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App";
+/*This file is used for client-side rendering of the React application using ReactDOM.
+It imports the necessary libraries and components, and renders them to the 'root' element in the HTML file.*/
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+import React from "react"; 
+import ReactDOM from "react-dom/client"; 
+import App from "./components/App"; 
+
+const root = ReactDOM.createRoot(document.getElementById("root")); 
+root.render( 
+<React.StrictMode>
+<App />
+</React.StrictMode>
 );
+
