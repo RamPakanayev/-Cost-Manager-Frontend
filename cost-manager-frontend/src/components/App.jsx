d6b80d36c4041aae6f2fe87af1a00a2830a46e73
RamPakanayev <rampakanayev@gmail.com>
2023-01-25 20:08:32 +0200

    Update App.jsx

--- cost-manager-frontend/src/components/App.jsx
+++ cost-manager-frontend/src/components/App.jsx
@@ -3,66 +3,72 @@ import LocalStorageLibrary from "../LocalStorage";
 import AddCostForm from "./AddCostForm";
 import ReportsForm from "./ReportsForm";
 
+// Main component that renders the AddCostForm and ReportsForm components
+// It also handles the data manipulation and communication with the LocalStorageLibrary
 function App() {
-  const [costItems, setCostItems] = useState([]);
-  const [report, setReport] = useState([]);
-  const [showReport, setShowReport] = useState(false);
-  const [month, setMonth] = useState(0);
-  const [year, setYear] = useState(0);
-  const [imagePath, setImagePath] = useState("./Logo.png");
+// State variables for the cost items, report data, and report display toggle
+const [costItems, setCostItems] = useState([]);
+const [report, setReport] = useState([]);
+const [showReport, setShowReport] = useState(false);
+const [month, setMonth] = useState(0);
+const [year, setYear] = useState(0);
+const [imagePath, setImagePath] = useState("./Logo.png");
 
-  useEffect(() => {
-    setImagePath(process.env.PUBLIC_URL + "/Logo.png");
-  }, []); // this will make the effect to be called only once when the component is rendered
+// useEffect hook that sets the imagePath state variable to the correct file path
+useEffect(() => {
+setImagePath(process.env.PUBLIC_URL + "/Logo.png");
+}, []); // this will make the effect to be called only once when the component is rendered
 
-  async function handleAddCostItem(sum, category, description) {
-    try {
-      if (category === "" || category === "Select a category")
-        category = "Other";
-      if (sum === "") window.alert("Please enter a sum");
-      else if (description === "") window.alert("Please enter a description");
-      else {
-        await LocalStorageLibrary.addCostItem({
-          sum,
-          category,
-          description,
-          date: new Date(),
-        });
-        const costItems = await LocalStorageLibrary.getCostItems();
-        setCostItems(costItems);
-        window.alert("The new cost has successfully added !");
-      }
-    } catch (error) {
-      console.error(error);
-    }
+// callback function that adds a new cost item to the local storage
+// It uses the LocalStorageLibrary tosave the new cost item and updates the costItems state variable with the updated data from the local storage
+async function handleAddCostItem(sum, category, description) {
+  try {
+  if (category === "" || category === "Select a category")
+  category = "Other";
+  if (sum === "") window.alert("Please enter a sum");
+  else if (description === "") window.alert("Please enter a description");
+  else {
+  await LocalStorageLibrary.addCostItem({
+  sum,
+  category,
+  description,
+  date: new Date(),
+  });
+  const costItems = await LocalStorageLibrary.getCostItems();
+  setCostItems(costItems);
+  window.alert("The new cost has successfully added !");
   }
-
+  } catch (error) {
+  console.error(error);
+  }
+  }
+  
+  // callback function that gets a report of the costs based on the selected month and year
+  // It uses the LocalStorageLibrary to get the report data and updates the report and showReport state variables
   async function handleGetReport(month, year) {
-    try {
-      const reportData = await LocalStorageLibrary.getReport(month, year);
-      setReport(reportData);
-      setShowReport(true);
-    } catch (error) {
-      console.error(error);
-    }
+  try {
+  const reportData = await LocalStorageLibrary.getReport(month, year);
+  setReport(reportData);
+  setShowReport(true);
+  } catch (error) {
+  console.error(error);
   }
-
+  }
+  
   return (
-    <div className="App">
-      <img src={imagePath} alt="Logo" />
-      <AddCostForm handleAddCostItem={handleAddCostItem} />
-
-      <ReportsForm
-        month={month}
-        setMonth={setMonth}
-        year={year}
-        setYear={setYear}
-        handleGetReport={handleGetReport}
-        showReport={showReport}
-        report={report}
-      />
-    </div>
-  );
+  <div className="App">
+  <img src={imagePath} alt="Logo" />
+  <AddCostForm handleAddCostItem={handleAddCostItem} />
+  <ReportsForm
+    month={month}
+    setMonth={setMonth}
+    year={year}
+    setYear={setYear}
+    handleGetReport={handleGetReport}
+    showReport={showReport}
+    report={report}
+  />
+</div>);
 }
 
 export default App;
